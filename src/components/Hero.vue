84045a81519813f6fb16b613f3cf19f50cefef24
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-11-10 17:25:24 +0100

    add import bootstrap in main.js

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,25 +1,21 @@
 <template>
   <div>
-    <div class="jumbotron">
-      <h1 class="display-4">Hello, world!</h1>
-      <p class="lead">
-        This is a simple hero unit, a simple jumbotron-style component for
-        calling extra attention to featured content or information.
-      </p>
-      <hr class="my-4" />
-      <p>
-        It uses utility classes for typography and spacing to space content out
-        within the larger container.
-      </p>
-      <p class="lead">
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-      </p>
+    <div class="jumbotron jumbotron-fluid py-5">
+      <div class="container">
+        <h1 class="display-4">Fluid jumbotron</h1>
+        <p class="lead">
+          This is a modified jumbotron that occupies the entire horizontal space
+          of its parent.
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Hero",
+};
 </script>
 
 <style>
